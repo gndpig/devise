7913f023a7e9211aeab16285d6af1e09735aa3a5
Taiki Nakatsuka <gnd.scandal.pig@gmail.com>
2012-12-10 12:07:33 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Devise::Application.config.secret_token = '95cc643bf585cb733e0d6a65a858a7f5c8e4f1259c43e7f5a8ce5b84231b13c806de2c37616571618c6e6f5018c234f3469039eb7d2ea4dc6c0751ab959c6992'
